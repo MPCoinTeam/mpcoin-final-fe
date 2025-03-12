c39d40360043a38add9dfb02767bdd70b4a5b967
vietddude <imrbdnv@gmail.com>
2025-03-12 14:36:59 +0700

    update: config

--- src/domain/usecases/hooks/configs/useConfig.ts
+++ src/domain/usecases/hooks/configs/useConfig.ts
@@ -2,7 +2,7 @@ import { NODE_ENV_DEFAULT } from '@/common/constants/Environments';
 
 const useConfig = () => ({
   nodeEnv: process.env.NODE_ENV || NODE_ENV_DEFAULT,
-  apiUrl: process.env.API_URL || 'http://192.168.0.102:5001/api/v1',
+  apiUrl: process.env.API_URL || 'http://10.29.160.137:5001/api/v1',
   binanceApiUrl: process.env.BINANCE_API_URL || 'https://api.binance.com/api/v3',
 });
 
